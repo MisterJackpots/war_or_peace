042ba447a188b7d7862498738cdb3392365accc3
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-08-18 11:50:05 -0700

    Refactor methods to account for no war card

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,7 @@
 class Turn
-  attr_reader :player1, :player2, :spoils_of_war
+  attr_reader :player1, 
+              :player2, 
+              :spoils_of_war
 
   def initialize(player1, player2)
     @player1 = player1
@@ -8,29 +10,33 @@ class Turn
   end
 
   def type
-    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+    @play_card1 = @player1.deck.rank_of_card_at(0)
+    @play_card2 = @player2.deck.rank_of_card_at(0)
+    @war_card1 = @player1.deck.rank_of_card_at(2)
+    @war_card2 = @player2.deck.rank_of_card_at(2)
+
+    if @play_card1 != @play_card2
       :basic
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
-      player1.deck.rank_of_card_at(2) != player2.deck.rank_of_card_at(2)
-      :war
-    else
+    elsif !@war_card1 || !@war_card2 || @war_card1 == @war_card2
       :mutually_assured_destruction
+    else
+      :war
     end
   end
 
   def winner
     if type == :basic
-      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
-        then player1
-      else player2
+      if @play_card1 > @play_card2
+        then @player1
+      else 
+        @player2
       end
-
     elsif type == :war
-      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
-        then player1
-      else player2
+      if @war_card1 > @war_card2
+        then @player1
+      else
+        @player2
       end
-
     else type == :mutually_assured_destruction
       "No Winner"
     end
@@ -38,17 +44,17 @@ class Turn
 
   def pile_cards
     if type == :basic
-      @spoils_of_war << player1.deck.remove_card
-      @spoils_of_war << player2.deck.remove_card
+      @spoils_of_war << @player1.deck.remove_card
+      @spoils_of_war << @player2.deck.remove_card
     elsif type == :war
       3.times do
-        @spoils_of_war << player1.deck.remove_card
-        @spoils_of_war << player2.deck.remove_card
+        @spoils_of_war << @player1.deck.remove_card
+        @spoils_of_war << @player2.deck.remove_card
       end
     else
       3.times do
-        player1.deck.remove_card
-        player2.deck.remove_card
+        @player1.deck.remove_card
+        @player2.deck.remove_card
       end
     end
   end
